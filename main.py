0f7c9e693e1dd9c93c7f3623df3af76ecdc7a414
ariel ashkenazy <ariel.ashkenazy@gmail.com>
2023-01-11 13:02:29 +0200

    updated function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import sqlite3
+import pandas as pd
 
 
 def create_db_test():
@@ -12,14 +13,14 @@ def create_db_test():
     # # Import data from a CSV file
     # with open('import.sql', 'r') as f:
     #     cursor.executescript(f.read())
-    # cursor.execute('''DROP TABLE metadata''')
-    #cursor.execute('''DROP TABLE experiment_Data''')
+    cursor.execute('''DROP TABLE metadata''')
+    cursor.execute('''DROP TABLE experiment_Data''')
 
-
-    # Create a table
+    # Create the metadata table
     cursor.execute('''CREATE TABLE metadata (series_number TEXT NOT NULL,
       sample_number TEXT NOT NULL,
       clinical_condition TEXT,
+      treatment TEXT,
       sex TEXT,
       age TEXT, 
       tissue TEXT,
@@ -30,7 +31,7 @@ def create_db_test():
       primary key (series_number, sample_number));
     ''')
 
-
+    # Create the experiment_Data table
     cursor.execute('''CREATE TABLE experiment_Data (series_number TEXT NOT NULL,
       sample_number TEXT NOT NULL,
       is_cell TEXT,
@@ -58,12 +59,26 @@ def create_db_test():
       ON UPDATE CASCADE );
     ''')
 
+    # TODO: should be in a loop that reads csv after csv and adds to growing 'metadata' database
+    # Read the CSV file into a DataFrame
+    metadata_df = pd.read_csv("GSE121638_metaData10X.csv")
 
-    # # Import data from a CSV file
-    # with open('GSE114724_metaData10X.csv', 'r') as f:
-    #     cursor.execute(f"DELETE FROM table1")
-    #     cursor.copy_from(f, 'table1', sep=',')
+    # TODO: should generalize it so every column name with space will have underscore instead
+    # renames all columns names so whitespace " " will be replaced with underscore "_"
+    # for example: from 'series number' to 'series_number' to match with the column in 'metadata' db
+    metadata_df.rename(columns={'series number': 'series_number'}, inplace=True)
+    metadata_df.rename(columns={'sample number': 'sample_number'}, inplace=True)
+    metadata_df.rename(columns={'clinical condition': 'clinical_condition'}, inplace=True)
+    metadata_df.rename(columns={'patient id': 'patient_id'}, inplace=True)
+    metadata_df.rename(columns={'cell type': 'cell_type'}, inplace=True)
 
+    # TODO: handle the case where a line in metadata CSV is empty line
+    # Insert the data into the metadata table
+    metadata_df.to_sql("metadata", conn, if_exists="append", index=False)
+
+    ## a test to check that it added the data to 'metadata' db
+    metadata_df_from_db = pd.read_sql_query("SELECT * from metadata", conn)
+    print(metadata_df_from_db)
 
     # Commit the transaction
     conn.commit()
