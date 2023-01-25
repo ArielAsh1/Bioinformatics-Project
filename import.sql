baf151f88c8010894544e91bcffff60978397076
ariel ashkenazy <ariel.ashkenazy@gmail.com>
2023-01-25 13:07:55 +0200

    moved metadata to a new file

--- import.sql
+++
@@ -1,3 +0,0 @@
-.mode csv
-.import GSE114724_metaData10X.csv table1
-.import GSE121638_metaData10X.csv table2
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sqlite3
 import pandas as pd
 import handle_contig
+import handle_metadata
 
 # Connect to the database
 conn = sqlite3.connect('new_database.db')
@@ -10,7 +11,7 @@ conn = sqlite3.connect('new_database.db')
 cursor = conn.cursor()
 
 
-def create_db_test():
+def create_db():
     # deletes previous existing tables
     # TODO: at the end, reconsider this drop, because we dont want to reload the whole db everytime...
     cursor.execute('''DROP TABLE metadata''')
@@ -62,52 +63,26 @@ def create_db_test():
       ON UPDATE CASCADE );
     ''')
 
-def append_meta():
-    # Loop through all the CSV files in the directory and appends their content to growing metadata db
-    # for each CSV file, only the content (without column names) is added.
-    # the column names appear only once, at the top of metadata database
-    for file_name in os.listdir('metadatas_csv_from_mobax'):
-        if file_name.endswith('.csv'):
-
-            # Read the CSV file into a DataFrame
-            metadata_df = pd.read_csv(os.path.join('metadatas_csv_from_mobax', file_name))
-
-            # rename all column names, so whitespace " " will be replaced with underscore "_"
-            # e.g: change 'series number' to 'series_number' so it matches with the column in our metadata db
-            metadata_df.rename(columns={'series number': 'series_number'}, inplace=True)
-            metadata_df.rename(columns={'sample number': 'sample_number'}, inplace=True)
-            metadata_df.rename(columns={'clinical condition': 'clinical_condition'}, inplace=True)
-            metadata_df.rename(columns={'patient id': 'patient_id'}, inplace=True)
-            metadata_df.rename(columns={'cell type': 'cell_type'}, inplace=True)
-
-            # if a line is empty (all NULL) so don't append it to metadata db
-            metadata_df.dropna(how='all', axis=0, inplace=True)
-
-            # Insert the data into the metadata table
-            metadata_df.to_sql("metadata", conn, if_exists="append", index=False)
-
-    # # Create an index on the foreign key columns
-    # cursor.execute('''CREATE INDEX series_contig_fk1 ON experiment_Data(series_number)''')
-    # cursor.execute('''CREATE INDEX sample_contig_fk2 ON experiment_Data(sample_number)''')
-
-    ## a test to check that it added the data to 'metadata' db
-    # metadata_df_from_db = pd.read_sql_query("SELECT * from metadata", conn)
-    # print(metadata_df_from_db)
-
 
 def append_contig(contig_df):
     contig_df.to_sql("experiment_Data", conn, if_exists="append", index=False)
 
 
 if __name__ == '__main__':
-    create_db_test()
+    create_db()
+    handle_metadata.create_metadata_db(conn)
+    # # a test to check that metadata table was created properly
+    # metadata_df_from_db = pd.read_sql_query("SELECT * from metadata", conn)
+    # print(metadata_df_from_db)
+
     contig_df = handle_contig.create_contig_df()
     append_contig(contig_df)
-    append_meta()
+
     # contig_df_from_db = pd.read_sql_query("SELECT * from experiment_data", conn)
     # print(contig_df_from_db)
     test_df = pd.read_sql_query("SELECT metadata.sample_number FROM metadata, experiment_data "
-                                "WHERE metadata.series_number = experiment_data.series_number AND metadata.sample_number = experiment_data.sample_number", conn)
+                                "WHERE metadata.series_number = experiment_data.series_number AND metadata.sample_number = experiment_data.sample_number",
+                                conn)
     # Commit the transaction
     print(test_df)
     conn.commit()
